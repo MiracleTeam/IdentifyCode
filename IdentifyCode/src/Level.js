70603d1ce9ac355dbb0d5702bd1d988e74faa699
ituuz <yue19870813@163.com>
2015-12-11 14:20:00 +0800

    First commit

---
+++ IdentifyCode/src/Level.js
@@ -0,0 +1,97 @@
+var level = level || {};
+
+
+level.all = {
+	L_1 : {
+	title:"白百合",
+	answer:[1,2,3,4]
+	},
+	L_2 : {
+		title:"便利贴",
+		answer:[1,3]
+	},
+	L_3 : {
+		title:"博斯普鲁斯海峡",
+		answer:[7]
+	},
+	L_4 : {
+		title:"茶几",
+		answer:[4,5]
+	},
+	L_5 : {
+		title:"创可贴",
+		answer:[3,8]
+	},
+	L_6 : {
+		title:"电线",
+		answer:[5,8]
+	},
+	L_7 : {
+		title:"范伟",
+		answer:[3,4,6]
+	},
+	L_8 : {
+		title:"房祖名",
+		answer:[3,7]
+	},
+	L_9 : {
+		title:"郭敬明",
+		answer:[1]
+	},
+	L_10 : {
+		title:"奖状",
+		answer:[1,5,6]
+	},
+	L_11 : {
+		title:"金字塔",
+		answer:[4]
+	},
+	L_12 : {
+		title:"刘翔",
+		answer:[2,5,7,8]
+	},
+	L_13 : {
+		title:"喷泉",
+		answer:[5]
+	},
+	L_14 : {
+		title:"三胖",
+		answer:[3,7,8]
+	},
+	L_15 : {
+		title:"双面胶",
+		answer:[1,5]
+	},
+	L_16 : {
+		title:"汪峰",
+		answer:[4]
+	},
+	L_17 : {
+		title:"杨臣刚",
+		answer:[1,3]
+	},
+	L_18 : {
+		title:"仪表盘",
+		answer:[3,5]
+	},
+	L_19 : {
+		title:"中国名胜",
+		answer:[1]
+	},
+	L_20 : {
+		title:"马",
+		answer:[3,4,5]
+	},
+	L_21 : {
+		title:"非exo成员",
+		answer:[1,2]
+	},
+	L_22 : {
+		title:"苍老师",
+		answer:[3,7]
+	},
+	L_23 : {
+		title:"狗狗",
+		answer:[1,2,4,6,7]
+	}
+}
---
+++ IdentifyCode/src/app.js
@@ -0,0 +1,279 @@
+
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+	timerTxt:null,
+	nextBtn:null,
+	titleSp:null,
+	titleTxt:null,
+	startNode:null,
+	leveNode:null,
+	answerList:null,
+	timer:30,
+	iconWidth:75,
+	currLevel:1,
+    ctor:function () {
+        this._super();
+        var size = cc.winSize;
+
+		this.createMainUI();
+        return true;
+    },
+	createMainUI:function () {
+		this.startNode = new cc.Node();
+		var startBtn = new ccui.Button("res/ui/start.png", "res/ui/start.png");
+		this.startNode.addChild(startBtn);
+		startBtn.addTouchEventListener(this.eventListen, this);
+		startBtn.name = "startBtn";
+		
+		var logo = new cc.Sprite("res/ui/logo.png");
+		this.startNode.addChild(logo);
+		logo.attr({
+            y: 100
+        });
+		
+		var size = cc.winSize;
+		this.startNode.attr({
+            x: size.width / 2,
+            y: size.height / 2
+        });
+		this.addChild(this.startNode);
+	},
+	timeHeart:function (dt) {
+		this.timer--;
+		this.timerTxt.setString("倒计时：" + this.timer);
+		if (this.timer <= 0) {
+			//失败
+			this.showResult();
+		}
+	},
+	eventListen:function (sender, type) {
+		if (type == ccui.Widget.TOUCH_ENDED){
+			if (sender.name == "startBtn") {
+				this.startNode.removeFromParent(true);
+				var size = cc.winSize;
+				this.sprite = new cc.Sprite(res.bg_png);
+				this.sprite.attr({
+					x: size.width / 2,
+					y: size.height / 2
+				});
+				this.addChild(this.sprite, 0);
+				
+				//title
+				this.titleSp = new cc.Sprite("res/ui/title.png");
+				this.titleSp.x = 200;
+				this.titleSp.y = 580;
+				this.addChild(this.titleSp);
+				
+				this.titleTxt = new cc.LabelTTF("Hello World", "Arial", 24);
+				this.titleTxt.x = this.titleSp.x + this.titleSp.width + 5;
+				this.titleTxt.y = 580;
+				this.titleTxt.anchorX = 0;
+				this.addChild(this.titleTxt);
+				this.titleTxt.setColor(cc.color(128,128,128));
+				
+				//timer
+				this.timerTxt = new cc.LabelTTF("倒计时：" + this.timer, "Arial", 24);
+				this.timerTxt.x = this.titleSp.x;
+				this.timerTxt.y = 650;
+				this.addChild(this.timerTxt);
+				
+				//下一关
+				this.nextBtn = new ccui.Button("res/ui/next.png", "res/ui/next.png");
+				this.nextBtn.addTouchEventListener(this.eventListen, this);
+				this.addChild(this.nextBtn);
+				this.nextBtn.name = "nextBtn";
+				this.nextBtn.x = 400;
+				this.nextBtn.y = 200;
+				this.answerList = new Array();
+				this.createItems(this.currLevel);
+				this.schedule(this.timeHeart,1, 30);
+			} else if (sender.name == "nextBtn") {
+				this.leveNode.removeFromParent(true);
+				var flag = this.checkAnswer();
+				this.currLevel++;
+				if (flag) {
+					if (this.currLevel > 23) {
+						//成功
+						this.showResult();
+						cc.log("购票成功");
+					} else {
+						this.answerList = new Array();
+						this.createItems(this.currLevel);
+					}
+				} else {
+					//失败
+					this.showResult();
+					cc.log("购票失败");
+				}
+			}
+		}
+	},
+	showResult:function () {
+		this.unschedule(this.timeHeart);
+		this.timerTxt.removeFromParent(true);
+		this.nextBtn.removeFromParent(true);
+		this.titleSp.removeFromParent(true);
+		this.titleTxt.removeFromParent(true);
+		
+		var count = this.currLevel - 2;
+		var size = cc.winSize;
+		if (count > 0) {
+			var succSp = new cc.Sprite("res/ui/succ.png");
+			this.addChild(succSp);
+			succSp.attr({
+				x: size.width / 2,
+				y: size.height / 2
+			});
+			
+			var countTxt = new cc.LabelTTF(count, "Arial", 17);
+			var percentTxt = new cc.LabelTTF("78%", "Arial", 17);
+			succSp.addChild(countTxt);
+			succSp.addChild(percentTxt);
+			countTxt.setColor(cc.color(0,0,0));
+			percentTxt.setColor(cc.color(0,0,0));
+			countTxt.attr({
+				x: 198,
+				y: 66
+			});
+			percentTxt.attr({
+				x: 130,
+				y: 32
+			});
+		} else {
+			var failSp = new cc.Sprite("res/ui/fail.png");
+			this.addChild(failSp);
+			failSp.attr({
+				x: size.width / 2,
+				y: size.height / 2
+			});
+		}
+		
+		var shareBtn = new ccui.Button("res/ui/share.png", "res/ui/share.png");
+		var againBtn = new ccui.Button("res/ui/again.png", "res/ui/again.png");
+		this.addChild(shareBtn);
+		this.addChild(againBtn);
+	},
+	anwserListener:function (sender, type) {
+		if (type == ccui.Widget.TOUCH_ENDED){
+			var boo = false;
+			for (var i = 0; i < this.answerList.length; i++) {
+				if (this.answerList[i] == sender.name) {
+					sender.removeAllChildren();
+					this.answerList.splice(i, 1);
+					boo = true;
+					break;
+				}
+			}
+			if (boo == false) {
+				this.answerList.push(sender.name);
+				var sp = new cc.Sprite("res/ui/logo.png");
+				sp.x = 20;
+				sp.y = 20;
+				sender.addChild(sp);
+			}
+		}
+	},
+	checkAnswer:function () {
+		var arr = level.all["L_" + this.currLevel].answer;
+		for (var i = 0; i < this.answerList.length; i++) {
+			var boo = false;
+			for (var j = 0; j < arr.length; j++) { 
+				if (this.answerList[i] == arr[j]) {
+					boo = true;
+				}
+			}
+			if (boo == false) {
+				return false;
+			}
+		}
+		for (var i = 0; i < arr.length; i++) {
+			var boo = false;
+			for (var j = 0; j < this.answerList.length; j++) { 
+				if (arr[i] == this.answerList[j]) {
+					boo = true;
+				}
+			}
+			if (boo == false) {
+				return false;
+			}
+		}
+		return true;
+	},
+	createItems:function (le) {
+		this.titleTxt.setString(level.all["L_" + le].title);
+		this.leveNode = new cc.Node();
+		var arr = this.randomArray();
+
+		var sp1 = this.createItem(le, arr[0]);
+		this.leveNode.addChild(sp1);
+		
+		var sp2 = this.createItem(le, arr[1]);
+		this.leveNode.addChild(sp2);
+		sp2.x = sp1.x + this.iconWidth;
+		
+		var sp3 = this.createItem(le, arr[2]);
+		this.leveNode.addChild(sp3);
+		sp3.x = sp2.x + this.iconWidth;
+		
+		var sp4 = this.createItem(le, arr[3]);
+		this.leveNode.addChild(sp4);
+		sp4.x = sp3.x + this.iconWidth;
+		
+		var sp5 = this.createItem(le, arr[4]);
+		this.leveNode.addChild(sp5);
+		sp5.y = sp1.y - this.iconWidth;
+		
+		var sp6 = this.createItem(le, arr[5]);
+		this.leveNode.addChild(sp6);
+		sp6.x = sp5.x + this.iconWidth;
+		sp6.y = sp5.y;
+		
+		var sp7 = this.createItem(le, arr[6]);
+		this.leveNode.addChild(sp7);
+		sp7.x = sp6.x + this.iconWidth;
+		sp7.y = sp5.y;
+		
+		var sp8 = this.createItem(le, arr[7]);
+		this.leveNode.addChild(sp8);
+		sp8.x = sp7.x + this.iconWidth;
+		sp8.y = sp5.y;
+		
+		var size = cc.winSize;
+		this.leveNode.attr({
+            x: 200,
+            y: size.height / 2
+        });
+		this.addChild(this.leveNode);
+	},
+	createItem:function (le, ind) {
+		var sp = new ccui.Button("res/level/" + le + "/" + ind + ".png", "res/level/" + le + "/" + ind + ".png");
+		sp.name = ind;
+		sp.addTouchEventListener(this.anwserListener, this);
+		return sp;
+	},
+	randomArray:function () {
+		var pool = [1, 2, 3, 4, 5, 6, 7, 8];
+		var arr2 = new Array();  
+		var count = pool.length;  
+		var cbRandCount = 0;// 索引  
+		var cbPosition = 0;// 位置  
+		var k = 0;  
+		do {  
+			var r = count - cbRandCount;  
+			cbPosition = Math.floor(r * Math.random());  
+			arr2.push(pool[cbPosition]);
+			cbRandCount++;  
+			pool[cbPosition] = pool[r - 1];
+		} while (cbRandCount < count);  
+		return arr2;  
+	}
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
